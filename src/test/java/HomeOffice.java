5743040ec19df79a63ec249b114faf327d2a961f
mehringl <limehring@hotmail.com>
2022-10-25 10:31:06 -0300

    Fix - Anda sólo para fecha de hoy

--- src/test/java/HomeOffice.java
+++ src/test/java/HomeOffice.java
@@ -6,41 +6,33 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
+import java.time.LocalDate;
 import java.time.Duration;
 
 public class HomeOffice {
 
     @Test(priority = 1, description = "Cargar ausentismo HomeOffice")
     public void cargarHomeOffice() {
-
+        int diaHoy = LocalDate.now().getDayOfMonth();
         System.setProperty("webdriver.edge.driver","C:\\Users\\mehringl\\IdeaProjects\\Automatización HomeOffice\\src\\main\\resources\\drivers\\msedgedriver.exe");
-
         // Iniciar navegador
         WebDriver driver=new EdgeDriver();
-
         // Abrir Portal Colaborador
         driver.get("https://portaldelcolaborador.bpn.com.ar/Ausentismo/RegistroAusentismo");
         // Maximizar ventana
         driver.manage().window().maximize();
-
         driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/a[1]/span[2]")).click();
-
-
-        //Generic Wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         Select selectMotivo=new Select(driver.findElement(By.xpath("//*[@id=\"tipoAusentismo\"]")));
         WebElement motivo= driver.findElement(By.xpath("//*[@id=\"tipoAusentismo\"]"));
-
-        //Wait after locator is load
         wait.until(ExpectedConditions.visibilityOf(motivo));
-
-       //selectMotivo.click();
-
         selectMotivo.selectByVisibleText("Home Office");
-
-        driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/form/div/div[10]/div[2]/button")).click();
-
+        WebElement selectFecha=driver.findElement(By.xpath("//*[@id=\"lblHasta\"]"));
+        int diaSelect=Integer.parseInt(selectFecha.getText().substring(0,2));
+        if (diaSelect==diaHoy){
+            driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/form/div/div[10]/div[2]/button")).click();
+        } else {
+            System.out.println("No es el mismo día");
+        }
     }
 }
